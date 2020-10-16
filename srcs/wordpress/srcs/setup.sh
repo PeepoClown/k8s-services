dfc0f85f5ea6db86efbed9bc287076526637d914
Dmitrij Vasew <wupdegra@ox-s5.msk.21-school.ru>
2020-10-16 18:07:44 +0300

    wp almost done

--- srcs/wordpress/srcs/setup.sh
+++ srcs/wordpress/srcs/setup.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-# rc default
-# /usr/bin/supervisord -c /etc/supervisord.conf
-php --server 0.0.0.0:5050 --docroot /www/
-#php -S 0.0.0.0:5050 -t /www/wordpress/
+/usr/bin/supervisord -c /etc/supervisord.conf
