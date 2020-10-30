04d2e4f4fd1f43673d88e113d2aa790991489caa
PeepoClown <dvasev09@gmail.com>
2020-10-30 20:09:05 +0300

    asdas

--- srcs/grafana/srcs/setup.sh
+++ srcs/grafana/srcs/setup.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-./grafana-server
+# ./grafana-server
+
+mkdir /etc/telegraf
+cp /etc/telegraf.conf /etc/telegraf/telegraf.conf
+sed -i "s~# database = \"telegraf\"~database = \"grafana\"~g" /etc/telegraf/telegraf.conf
+sed -i "s~# urls = \[\"unix:///var/run/influxdb.sock\"\]~urls = \[\"http://influxdb:8086\"\]~g" /etc/telegraf/telegraf.conf
+telegraf &
+
+./bin/grafana-server
