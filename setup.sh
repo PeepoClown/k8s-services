45b4795115016e6191c6f264aa914c3b900eec1b
Eblomeshatel <dvasev09@gmail.com>
2020-11-05 16:24:09 +0300

    mysql wp pma works

--- setup.sh
+++ setup.sh
@@ -41,34 +41,34 @@ complete_msg "finished!"
 
 eval $(minikube docker-env)
 
-kubectl apply -f srcs/metallb-config.yaml > /dev/null
-complete_msg "load balancer configured!"
-
-kubectl create secret generic -n metallb-system memberlist \
-	--from-literal=secretkey="$(openssl rand -base64 128)" > /dev/null
-complete_msg "kubernetes secret generated!"
-
 start_msg "building images..."
-build_image "ftps image" ftps_image srcs/ftps
-build_image "nginx image" nginx_image srcs/nginx
+# build_image "ftps image" ftps_image srcs/ftps
+# build_image "nginx image" nginx_image srcs/nginx
 build_image "mysql image" mysql_image srcs/mysql
 build_image "wordpress image" wordpress_image srcs/wordpress
 build_image "phpMyAdmin image" php_my_admin_image srcs/php_my_admin
-build_image "influxdb image" influxdb_image srcs/influxdb
-build_image "grafana image" grafana_image srcs/grafana
+# build_image "influxdb image" influxdb_image srcs/influxdb
+# build_image "grafana image" grafana_image srcs/grafana
 complete_msg "finished!"
 
+kubectl apply -f srcs/metallb-config.yaml > /dev/null
+complete_msg "load balancer configured!"
+
 start_msg "setup services..."
-setup_service "ftps service" srcs/ftps.yaml
-setup_service "nginx service" srcs/nginx.yaml
+# setup_service "ftps service" srcs/ftps.yaml
+# setup_service "nginx service" srcs/nginx.yaml
 setup_service "mysql volume" srcs/mysql-pv.yaml
 setup_service "mysql service" srcs/mysql.yaml
 setup_service "wordpress service" srcs/wordpress.yaml
 setup_service "phpMyAdmin service" srcs/php_my_admin.yaml
-setup_service "influxdb volume" srcs/influxdb-pv.yaml
-setup_service "influxdb service" srcs/influxdb.yaml
-setup_service "grafana service" srcs/grafana.yaml
+# setup_service "influxdb volume" srcs/influxdb-pv.yaml
+# setup_service "influxdb service" srcs/influxdb.yaml
+# setup_service "grafana service" srcs/grafana.yaml
 complete_msg "finished!"
+
+kubectl create secret generic -n metallb-system memberlist \
+	--from-literal=secretkey="$(openssl rand -base64 128)" > /dev/null
+complete_msg "kubernetes secret generated!"
 
 echo -e "\n\n"
 complete_msg "All Done!!!"
--- srcs/mysql/srcs/setup.sh
+++ srcs/mysql/srcs/setup.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
+openrc default
+
+/etc/init.d/mariadb setup
+/etc/init.d/mariadb start
+
+mysql -u root < /var/mysql_init.sql
+mysql wordpress -u root < /var/mysql_wordpress.sql
+
+/etc/init.d/mariadb stop
+
 /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
+
+# rc-serice
