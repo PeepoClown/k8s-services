8a10df9e4e276495b4132f38644a0e1a9129585d
Eblomeshatel <dvasev09@gmail.com>
2020-11-11 21:21:53 +0300

    a chto zvuchit revers-proxivno

--- srcs/php_my_admin/srcs/phpmyadmin.php
+++ srcs/php_my_admin/srcs/phpmyadmin.php
@@ -149,6 +149,8 @@ $cfg['SaveDir'] = '';
  */
 //$cfg['SendErrorReports'] = 'always';
 
+$cfg['TempDir'] = '/tmp';
+
 /**
  * You can find more configuration options in the documentation
  * in the doc/ folder or at <https://docs.phpmyadmin.net/>.
