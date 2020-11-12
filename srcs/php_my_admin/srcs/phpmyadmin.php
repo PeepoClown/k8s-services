10560ba018535573b778f0f09a551047a0f83239
Eblomeshatel <dvasev09@gmail.com>
2020-11-12 21:10:41 +0300

    ebat srabotalo

--- srcs/php_my_admin/srcs/phpmyadmin.php
+++ srcs/php_my_admin/srcs/phpmyadmin.php
@@ -149,9 +149,10 @@ $cfg['SaveDir'] = '';
  */
 //$cfg['SendErrorReports'] = 'always';
 
-$cfg['TempDir'] = '/tmp';
-
 /**
  * You can find more configuration options in the documentation
  * in the doc/ folder or at <https://docs.phpmyadmin.net/>.
  */
+
+// $cfg['AllowThirdPartyFraming'] = true;
+$cfg['TempDir'] = '/tmp';
